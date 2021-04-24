3626bb137469d5a94ca792482f1dacd1b1219832
Anmolbir Singh Mann <sikhanmol@yahoo.com>
2021-04-24 10:41:26 -0700

    saving progress

--- main.py
+++ main.py
@@ -11,10 +11,21 @@ def main(args):
     print(f'\nBeginning experiments, provided arguments: {args}\n')
     problem_name = "Forest" if not args.lake else "Frozen_Lake"
     P, R = example.forest(S=10, r1=4, r2=2, p=0.25, is_sparse=False) # Forest 
+    alphas = [0.1, 0.001]
+    epsilons = [0.95, 0.9]
 
     gammas = [0.8, 0.9, 0.95, 0.99]
-    run_iterations(P, R, gammas=gammas, problem_name=problem_name, value_iter=True) # value iteration
-    run_iterations(P, R, gammas=gammas, problem_name=problem_name, value_iter=False) # policy iteration
+    kargs = {
+        "P": P, 
+        "R": R,
+        "problem_name": problem_name,
+        "output": args.output
+    }
+    run_iterations(gammas=gammas, value_iter=True, **kargs) # value iteration
+    run_iterations(gammas=gammas, value_iter=False, **kargs) # policy iteration
+    run_qlearnings(params=alphas, n_iter=1000000, param_alpha=True, **kargs) # qlearning alphas
+    run_qlearnings(params=epsilons, n_iter=1000000, param_alpha=False, **kargs) # qlearnings 
+    q_learning(plot=True, **kargs)
 
     print(f'\nCompleted!\narguments: {args}\n')
     
--- mdp.py
+++ mdp.py
@@ -10,59 +10,79 @@ from hiive.mdptoolbox import mdp, example
 RANDOM_SEED = 1994540101
 np.random.seed(RANDOM_SEED) # keep results consistent
 
-def run_iterations(P, R, gammas=[0.8, 0.99], problem_name="Forest", value_iter=True, output="output", show=False):
+def run_iterations(P, R, gammas=[0.99, 0.9, 0.85, 0.8], problem_name="Forest", value_iter=True, output="output", show=False):
     policies = {}
     rewards = {}
-    time = {}
+    time = []
     Iteration = mdp.ValueIteration if value_iter else mdp.PolicyIteration
     desc = "Value_Iteration" if value_iter else "Policy_Iteration"
     for gamma in gammas:
         it = Iteration(P, R, gamma)
         results = it.run()
         col = f'Gamma: {gamma}'
-        iterations = list(range(1, len(results) + 1))
-        rewards[col] = pd.Series([it['Reward'] for it in results], index=iterations)
-        time[col] = pd.Series([it['Time'] for it in results], index=iterations)
+        iterations = [it['Iteration'] for it in results]
+        rewards[col] = pd.Series([it['Mean V'] for it in results], index=iterations)
+        time.append(it.time)
         policies[col] = it.policy
     rewards = pd.DataFrame(rewards)
     rewards.index.name = 'Iterations'
-    time = pd.DataFrame(time)
-    time.index.name = 'Iterations'
+    time = pd.DataFrame(time, index=gammas)
+    time.index.name = 'Gamma'
     
     
     # plot and log results
     rewards.plot()
-    plt.title(f"{problem_name}: {desc}: Max Utility over Iterations")
-    plt.ylabel("Max Utility")
+    plt.title(f"{problem_name}: {desc}: Mean Utility over Iterations")
+    plt.ylabel("Mean Utility")
     plt.tight_layout()
     plt.savefig(f"{output}/{problem_name}-{desc}-utility.png")
     if show:
         plt.plot()
     
-    time.plot()
-    plt.title(f"{problem_name}: {desc}: Time over Iterations")
+    time.plot(legend=False)
+    plt.title(f"{problem_name}: {desc}: Time for different gammas")
     plt.ylabel("Time")
     plt.tight_layout()
     plt.savefig(f"{output}/{problem_name}-{desc}-time.png")
     if show:
         plt.plot()
+    else:
+        plt.close()
     
     print(f"Problem: {problem_name}\nFunction: {desc}\nPolicies for different gamma values:\n{policies}")
     return rewards, time, policies
 
 
-def q_learning(P, R, gamma=0.99 ,alpha=0.1, alpha_decay=0.99, alpha_min=0.001, e=1.0, e_min=0.1, e_decay=0.99, n_iter=10000):
+def q_learning(P, R, gamma=0.99 ,alpha=0.1, alpha_decay=0.99, alpha_min=0.001, epsilon=1.0, e_min=0.1, e_decay=0.999, n_iter=10000, plot=False, show=False, output="output", problem_name="Forest"):
     args = {
         "alpha": alpha,
         "alpha_decay": alpha_decay,
         "alpha_min": alpha_min,
-        "epsilon": e,
+        "epsilon": epsilon,
         "epsilon_min": e_min,
         "epsilon_decay": e_decay,
         "n_iter": n_iter
     }
     ql = mdp.QLearning(P, R, gamma, **args) 
     ql_results = ql.run()
+
+    if plot:
+        rewards = [i['Mean V'] for i in ql_results]
+        iterations = [i['Iteration'] for i in ql_results]
+        desc = 'Q-Learning'
+
+        # plot and log results
+        plt.clf()
+        plt.plot(iterations, rewards)
+        plt.title(f"{problem_name}: {desc}: Mean Utility over Iterations")
+        plt.ylabel("Mean Utility")
+        plt.xlabel("Iterations")
+        plt.tight_layout()
+        plt.savefig(f"{output}/{problem_name}-{desc}-utility.png")
+        if show:
+            plt.plot()
+        else:
+            plt.close()
     return ql, ql_results
 
 def run_qlearnings(P, R, params=[0.1, 0.25], problem_name="Forest", value_iter=True, output="output", param_alpha=True, show=False, n_iter=10000):
@@ -75,8 +95,8 @@ def run_qlearnings(P, R, params=[0.1, 0.25], problem_name="Forest", value_iter=T
     for param in params:
         q, results = q_learning(P, R, n_iter=n_iter, **{param_name: param})
         col = f'{param_name}: {param}'
-        iterations = list(range(1, len(results) + 1))
-        rewards[col] = pd.Series([it['Max V'] for it in results], index=iterations)
+        iterations = [it['Iteration'] for it in results]
+        rewards[col] = pd.Series([it['Mean V'] for it in results], index=iterations)
         time[col] = pd.Series([it['Time'] for it in results], index=iterations)
         policies[col] = q.policy
         
@@ -87,12 +107,14 @@ def run_qlearnings(P, R, params=[0.1, 0.25], problem_name="Forest", value_iter=T
     
     # plot and log results
     rewards.plot()
-    plt.title(f"{problem_name}: {desc}: Max Utility over Iterations")
-    plt.ylabel("Max Utility")
+    plt.title(f"{problem_name}: {desc}: Mean Utility over Iterations")
+    plt.ylabel("Mean Utility")
     plt.tight_layout()
     plt.savefig(f"{output}/{problem_name}-{desc}-utility.png")
     if show:
         plt.plot()
+    else:
+        plt.close()
     
     time.plot()
     plt.title(f"{problem_name}: {desc}: Time over Iterations")
@@ -101,6 +123,8 @@ def run_qlearnings(P, R, params=[0.1, 0.25], problem_name="Forest", value_iter=T
     plt.savefig(f"{output}/{problem_name}-{desc}-time.png")
     if show:
         plt.plot()
+    else:
+        plt.close()
         
     print(f"Problem: {problem_name}\nFunction: {desc}\nPolicies for different alphas values:\n{policies}")
     return rewards, time, policies
